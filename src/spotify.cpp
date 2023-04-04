93d5ebaf7ec7b4fe67a0f995d2764403f140cb5e
HutchyBean <benhutchy123456@gmail.com>
2023-04-05 00:03:44 +0100

    Get Access Token

--- src/spotify.cpp
+++ src/spotify.cpp
@@ -18,14 +18,13 @@ std::string Spotify::GetUserAuth() {
 
   // Define paramaters
   std::string scope = "user-read-playback-state user-modify-playback-state user-read-currently-playing playlist-read-private playlist-read-collaborative";
-  std::string redirect_uri = "http://localhost:8888/callback";
 
   // Create URL
   std::ostringstream url_stream;
   url_stream << "https://accounts.spotify.com/authorize?response_type=code";
   url_stream << "&client_id=" << httplib::detail::encode_query_param(client_id);
-  url_stream << "&redirect_uri=" << httplib::detail::encode_query_param(redirect_uri);
-  url_stream << "&scope=" << scope;
+  url_stream << "&redirect_uri=" << httplib::detail::encode_query_param("http://localhost:8888/callback");
+  url_stream << "&scope=" << httplib::detail::encode_query_param(scope);
   std::string url = url_stream.str();
   
   // Display URL and start callback
@@ -33,6 +32,33 @@ std::string Spotify::GetUserAuth() {
   callback.listen("localhost", 8888);
   return code;
 }
+AccessTokenRes Spotify::GetAccessToken(std::string code) {
+  std::ostringstream form;
+  form << "grant_type=authorization_code";
+  form << "&redirect_uri=" << httplib::detail::encode_query_param("http://localhost:8888/callback");
+  form << "&code=" << httplib::detail::encode_query_param(code);
+
+  httplib::Headers headers = {
+    {"Authorization", "Basic " + httplib::detail::base64_encode(this->client_id + ":" + this->client_secret)}
+  };
+
+  auto res = this->accounts.Post("/api/token", headers, form.str(), "application/x-www-form-urlencoded");
+  if (res->status != 200) throw("error getting access token.");
+
+  rapidjson::Document json;
+  AccessTokenRes response;
+
+  json.Parse(res->body.c_str());
+
+  response.access_token = json["access_token"].GetString();
+  response.scope = json["scope"].GetString();
+  response.refresh_token = json["refresh_token"].GetString();
+  response.token_type = json["token_type"].GetString();
+  response.expires = json["expires_in"].GetInt();
+
+  return response;
+}
+
 
 Spotify::Spotify(const char* config){
   std::ifstream config_file("config.json");
@@ -44,5 +70,9 @@ Spotify::Spotify(const char* config){
   client_id = json["client_id"].GetString();
   client_secret = json["client_secret"].GetString();
   
-  std::string authCode = GetUserAuth();
+  std::string auth_code = GetUserAuth();
+  AccessTokenRes access_stuff = GetAccessToken(auth_code);
+  this->access_token = access_stuff.access_token;
+  this->refresh_token = access_stuff.refresh_token;
 } 
+
--- src/spotify.h
+++ src/spotify.h
@@ -3,6 +3,14 @@
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include <httplib.h>
 
+struct AccessTokenRes {
+  std::string access_token;
+  std::string token_type;
+  std::string scope;
+  int expires;
+  std::string refresh_token;
+};
+
 class Spotify {
 private:
   httplib::SSLClient api{"api.spotify.com"};
@@ -11,7 +19,10 @@ private:
   std::string refresh_token;
   std::string client_id;
   std::string client_secret;
+
   std::string GetUserAuth();
+  AccessTokenRes GetAccessToken(std::string);
+
 public:
   Spotify(const char* config = "config.json");
 };
